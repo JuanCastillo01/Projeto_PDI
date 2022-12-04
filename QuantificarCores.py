ae8117b4e97d7918ef2c6c31b96aff1526d2afe8
JuanCastillo01 <84253622+JuanCastillo01@users.noreply.github.com>
2022-12-04 11:50:08 -0300

    3 Conjuntos de imagens quantificadas em 5,10 e 15 cores

--- QuantificarCores.py
+++ QuantificarCores.py
@@ -3,6 +3,8 @@ import os
 import cv2
 
 
+# Função para quantificar imagens coloridas
+
 def clr_Quant(image, k):
     i = np.float32(image).reshape(-1, 3)
     condition = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
@@ -12,13 +14,21 @@ def clr_Quant(image, k):
     qnt_Image = qnt_Image.reshape(image.shape)
     return qnt_Image
 
-
+# Lista de arquivos na pasta
 samples = os.listdir("Sampled Images")
 
+# Opera todos os arquivos na pasta
 for path in samples:
     etapaA = cv2.imread(f"Sampled Images/{path}")
-    cv2.imshow(path,etapaA)
-    cv2.waitKey(800)
-    output = clr_Quant(etapaA,5)
-    cv2.imshow(path,output)
-    cv2.waitKey(800)
+    # cv2.imshow(path, etapaA)
+    # cv2.waitKey(10)
+
+    k = 15
+    output = clr_Quant(etapaA, k)
+
+    # cv2.imshow(path, output)
+    # cv2.waitKey(10)
+
+
+    # Salvar imagens
+    cv2.imwrite(f"Quantified Photos/K{k}_{path}", output)
